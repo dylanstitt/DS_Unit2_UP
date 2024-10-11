e95c9caa58c74094cce9ab256700fbd71fb6193f
Dylan Stitt <dstitt527@gmail.com>
2024-10-11 13:55:25 -0400

    debugging

--- ecosystem.py
+++ ecosystem.py
@@ -14,6 +14,8 @@ class River:
 
         self.__pendingBabies = []
 
+        self.__initialPopulation()
+
     def __str__(self):
         """Print the river"""
         for i in self.river:
@@ -27,7 +29,7 @@ class River:
         return self.river[x]
 
     ############################################# MAKE PRIVATE AND RUN IN NEW DAY
-    def initialPopulation(self, bearLives):
+    def __initialPopulation(self):
         """Place the initial population of bears and fish randomly in the river"""
         used = []
         for i in range(self.numBears):
@@ -36,7 +38,7 @@ class River:
                 x, y = randint(0, self.size-1), randint(0, self.size-1)
 
             used.append((x, y))
-            bear = Bear(x, y, bearLives)
+            bear = Bear(x, y, 5)
             self.river[y][x] = bear.icon
             self.animals.append(bear)
 
@@ -52,7 +54,6 @@ class River:
 
         self.population = len(self.animals)
 
-
     def addBaby(self, baby):
         self.__pendingBabies.append(baby)
 
@@ -64,18 +65,23 @@ class River:
 
         self.population = len(self.animals)
 
-    def animalDeath(self):
+    def animalDeath(self, animal):
         """Kills and removes and animals from the river"""
-        ...
+        self.animals.remove(animal)
+        self.population = len(self.animals)
 
-    def redraw(self):
+        self.river[animal.y][animal.x] = '🟦'
+
+    def redraw(self, animal, currentX, currentY, newCoords):
         """Redraws the river to the correct display"""
-        ...
+
+        self.river[newCoords[1]][newCoords[0]] = animal.icon
+        self.river[currentY][currentX] = '🟦'
 
     def newDay(self):
         """Main simulation functionality"""
-        ...
-        # Call place baby in here
+        for animal in self.animals:
+            animal.move(self)
 
 ###################################################
 
@@ -87,12 +93,14 @@ class Animal:
         self.y = y
         self.bredToday = False
         self.icon = ''
+        self.maxLives = 0
 
     def death(self, river):
         """Calls the river animalDeath function and passes in the current object"""
         river.animalDeath(self)
 
     def move(self, river):
+        print(river)
         """Move the current animal to a new position"""
         orignial = [(self.x - 1, self.y), (self.x + 1, self.y), (self.x, self.y - 1), (self.x, self.y + 1), (self.x - 1, self.y - 1), (self.x + 1, self.y + 1), (self.x - 1, self.y + 1), (self.x + 1, self.y - 1)]
         available = []
@@ -103,38 +111,64 @@ class Animal:
                 elif coords[1] < 0 or coords[1] > river.size-1:
                     continue
 
-                if river[coords[1]][coords[0]] != '🟦':
-                    continue
                 available.append(coords)
 
             except IndexError:
                 continue
 
-        if len(available) != 0:
-            currentCoords = (self.x, self.y)
-            newCoords = choice(available)
+        ####################### FIX THE COLLISION OF ANIMALS WHEN THEY WANT TO MOVE SOMEWHERE
+        ####################### PUT ALL COLLISION LOGIC IN THE COLLISION FUNCTION
+        '''for coords in available:
+            if river[coords[1]][coords[0]] == '🐟':
+                fishObj = []
+                fish = 0
+                for i in range(coords[1]):
+                    for j in range(coords[0]):
+                        if river[i][j] == '🐟':
+                            fish += 1
 
-            river[newCoords[1]][newCoords[0]] = self.icon
-            river[currentCoords[1]][currentCoords[0]] = '🟦'
+                for a in river.animals:
+                    if a.icon == '🐟':
+                        fish -= 1
+                        fishObj.append(a)
 
-    def collision(self, river, other):
+                        if fish == 0:
+                            break
+
+                self.collision(river, other=fishObj[-1])
+
+            else:
+                self.collision(river, otherIcon='🐻')
+
+            river.redraw(self, self.x, self.y, coords)'''
+
+
+    def collision(self, river, other=None, otherIcon=''):
         """Detect collision between Bears and Fish and same sex animals"""
-        if self.icon == other.icon:
-            available = [(self.x-1, self.y), (self.x+1, self.y), (self.x, self.y-1), (self.x, self.y+1), (self.x-1, self.y-1), (self.x+1, self.y+1), (self.x-1, self.y+1), (self.x+1, self.y-1)]
-            for coords in available:
+        if self.icon == otherIcon:
+            orignial = [(self.x - 1, self.y), (self.x + 1, self.y), (self.x, self.y - 1), (self.x, self.y + 1), (self.x - 1, self.y - 1), (self.x + 1, self.y + 1), (self.x - 1, self.y + 1), (self.x + 1, self.y - 1)]
+            available = []
+            for coords in orignial:
                 try:
+                    if coords[0] < 0 or coords[0] > river.size - 1:
+                        continue
+                    elif coords[1] < 0 or coords[1] > river.size - 1:
+                        continue
+
                     if river[coords[1]][coords[0]] != '🟦':
-                        available.remove(coords)
+                        continue
+                    available.append(coords)
 
                 except IndexError:
                     continue
-            print(available) #####################################################
-            if self.icon == '🐻':
-                coords = choice(available)
-                river.addBaby(Bear(coords[0], coords[1], other.maxLives))
-            else:
-                coords = choice(available)
-                river.addBaby(Fish(coords[0], coords[1]))
+
+            if len(available) != 0:
+                if self.icon == '🐻':
+                    coords = choice(available)
+                    river.addBaby(Bear(coords[0], coords[1], self.maxLives))
+                else:
+                    coords = choice(available)
+                    river.addBaby(Fish(coords[0], coords[1]))
 
         else:
             Bear.consume(self, other, river)
@@ -161,12 +195,13 @@ class Bear(Animal):
         if not self.eatenToday:
             self.lives -= 1
 
-        if self.lives == 0:
-            self.death(river)
+            if self.lives == 0:
+                self.death(river)
 
     def consume(self, fish, river):
         """Bear eating a fish and removing the fish from the river"""
         self.lives = self.maxLives
+        self.eatenToday = True
         fish.death(river)
 
 ###################################################
--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 # Unit 2 Unit Project
 # Bear Fish River
 
+#TODO - Finish the newDay function for the simulation running and fix the collision logic
+#TODO - debug
+
 from ecosystem import *
-import time, os, random
+from time import sleep
 
 DAYS_SIMULATED = 30
 RIVER_SIZE = 10
@@ -11,50 +14,21 @@ START_BEARS = 10
 START_FISH = 10
 
 def main():
-    river = River(RIVER_SIZE, START_BEARS, START_FISH)
-    #river.initialPopulation(5)
-    #print(river)
-    for i in range(10):
-        fish = Fish(random.randint(0, river.size - 1), random.randint(0, river.size - 1))
-        river.addBaby(fish)
 
-    river.placeBaby()
-    #print(river)
-    for fish in river.animals:
-        fish.move(river)
-    #print(river)
-
-
-if __name__ == '__main__':
-    for i in range(1000000):
-        main()
-
-
-# BACKUP
-'''
-from ecosystem import *
-from time import sleep
-
-DAYS_SIMULATED = 30
-RIVER_SIZE = 15
-START_BEARS = 10
-START_FISH = 10
-
-def BearFishRiver():
-
-  r = River(RIVER_SIZE, START_BEARS, START_FISH)
-  day = 0
-  done = False
-  for day in range(DAYS_SIMULATED):
-    print(f"\n\nDay: {day+1}")
-    print(r)
-    print(f"\nStarting Poplation: {r.population} animals")
-    done = r.new_day()
-    print(f"Ending Poplation: {r.population} animals")
-    print(r)
-    day += 1
-    sleep(5)
+    r = River(RIVER_SIZE, START_BEARS, START_FISH)
+    day = 0
+    done = False
+    for day in range(DAYS_SIMULATED):
+        print(f"\n\nDay: {day+1}")
+        print(r)
+        print(f"\nStarting Population: {r.population} animals")
+        done = r.newDay()
+        print(f"Ending Population: {r.population} animals")
+        print(r)
+        day += 1
+        #sleep()
 
 if __name__ == "__main__":
-  BearFishRiver()
-'''
+    '''for i in range(10000):
+        main()'''
+    main()
